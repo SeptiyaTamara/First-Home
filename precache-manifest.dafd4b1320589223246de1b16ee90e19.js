76107065bcdc19bd7dc742ebfb746ea9bb0d419e
SeptiyaTamara <septiyakristin98@mail.ugm.ac.id>
2019-03-14 08:10:07 +0700

    Updates

---
+++ precache-manifest.dafd4b1320589223246de1b16ee90e19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/First-Home/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "370616d6a349fb8ec5a7",
+    "url": "/First-Home/static/js/runtime~main.1a6feff4.js"
+  },
+  {
+    "revision": "24acfd938eb95afc70c9",
+    "url": "/First-Home/static/js/main.cbac5339.chunk.js"
+  },
+  {
+    "revision": "86208f7e04e8580d2c29",
+    "url": "/First-Home/static/js/2.e8ef8b6b.chunk.js"
+  },
+  {
+    "revision": "24acfd938eb95afc70c9",
+    "url": "/First-Home/static/css/main.9c3c4a8b.chunk.css"
+  },
+  {
+    "revision": "8607c62c2a65b20fed1effef70b590c5",
+    "url": "/First-Home/index.html"
+  }
+];
