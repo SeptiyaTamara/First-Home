fe028109cfb5a5a852b67c47d018998ad1239177
SeptiyaTamara <septiyakristin98@mail.ugm.ac.id>
2019-04-15 13:44:42 +0700

    Updates

---
+++ precache-manifest.a6fbe35fead4aa885fa68db006f02986.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/First-Home/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "370616d6a349fb8ec5a7",
+    "url": "/First-Home/static/js/runtime~main.1a6feff4.js"
+  },
+  {
+    "revision": "0166fd29e819b48286a4",
+    "url": "/First-Home/static/js/main.b5288223.chunk.js"
+  },
+  {
+    "revision": "3d4f18eb5243fe021075",
+    "url": "/First-Home/static/js/2.7ed9686b.chunk.js"
+  },
+  {
+    "revision": "0166fd29e819b48286a4",
+    "url": "/First-Home/static/css/main.5e72ba88.chunk.css"
+  },
+  {
+    "revision": "a8598f846a4fb3a3a327d8c61f41351e",
+    "url": "/First-Home/index.html"
+  }
+];
